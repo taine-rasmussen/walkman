871abbdfebe13bf64e8e8d8598fa6f443ef5a0b4
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-07-03 17:50:15 +1200

    add bootstrap

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+
+  const AUTH_URL =
+    "https://accounts.spotify.com/authorize?client_id=06f2f5fff93941138af657b367ed8576&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Login
