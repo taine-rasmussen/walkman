3b1f7d87e39e87d202cd2b9553dfc11bdaa784a9
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-07-03 18:12:18 +1200

    seasdf:

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 
 const Login = () => {
   const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=06f2f5fff93941138af657b367ed8576&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+    `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
   return (
     <Container
