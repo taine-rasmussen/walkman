b3db26549404541ed08c580b307d43e158f54e30
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-07-03 18:11:27 +1200

    server

---
+++ Server/Server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const spoitfyWebApi = requrire('spotify-web-api-node')
+
+const app = express()
+
+app.post('/login', (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'https://localhost:3000',
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET
+  })
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expires: data.body.expires_in
+      })
+    }).catch(() => {
+      res.sendStatus(400)
+    })
+})
