5c642cfb5b5c52c1bb37ffa4dbcb3a7967ffac12
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-07-06 19:06:23 +1200

    exposing keys makes it work?

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import { Container } from 'react-bootstrap'
 
 const Login = () => {
   const AUTH_URL =
-    `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+    `https://accounts.spotify.com/authorize?client_id=ee877ff172d84549ba81bbc86cd28478&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
   return (
     <Container
