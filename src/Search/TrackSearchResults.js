1e24a64a4361d45f9556345414dcf5f2f084d6e5
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-07-25 20:10:21 +1200

    filter out top result

--- src/Search/TrackSearchResults.js
+++ src/Search/TrackSearchResults.js
@@ -10,29 +10,40 @@ const TrackSearchResults = (props) => {
     chooseTrack(track)
   }
 
+  const topResult = searchResults[0]
+
   return (
     <>
-      {searchResults?.map(track => {
-        if (searchResults.indexOf(track) <= 1) { console.log(track) }
-
-
-
-        return (
-          < div
-            className="result_container"
-            onClick={() => { handlePlay(track) }}
-            style={{ cursor: "pointer" }}
-            key={track.uri}
-          >
-            <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
-            <div className="results_track">
-              <div className="result_primary">{track.title}</div>
-              <div className="result_secondary">{track.artist}</div>
-            </div>
+      <div className="container">
+        <div>
+          <h3>Top Result</h3>
+          <div>
+            <img src={topResult?.albumUrl} style={{ height: "64px", width: "64px" }} />
+            {topResult?.title}
+            {topResult?.artist}
           </div>
-        )
-      })
-      }
+        </div>
+        {searchResults?.map(track => {
+          if (searchResults.indexOf(track) == 0) return;
+          return (
+            <>
+              < div
+                className="result_container"
+                onClick={() => { handlePlay(track) }}
+                style={{ cursor: "pointer" }}
+                key={track.uri}
+              >
+                <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
+                <div className="results_track">
+                  <div className="result_primary">{track.title}</div>
+                  <div className="result_secondary">{track.artist}</div>
+                </div>
+              </div>
+            </>
+          )
+        })
+        }
+      </div>
     </>
   )
 }
