822544d6fdaf55a74a6465f782669c3a13892441
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-08-08 19:02:20 +1200

    sizing for body component

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BsSpotify } from 'react-icons/bs'
+
 import './Body.css'
 
 const Body = () => {
   return (
     <div className='body_container'>
-      this is the body
+      <BsSpotify />
     </div>
   )
 }
